f494435ed00c428eb025171bb53fd3f1694c57d9
KaleemAhmed123 <shaamidreez@gmail.com>
2023-04-05 19:23:10 +0530

    router setup done (initial).

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Saved = () => {
-  return <div>Saved</div>;
+  return (
+    <div>
+      <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+        <Outlet />
+      </section>
+    </div>
+  );
 };
 
 export default Saved;
--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  return (
+    <div>
+      <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+        <Outlet />
+      </section>
+    </div>
+  );
 };
 
 export default Trending;
