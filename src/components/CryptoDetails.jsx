612029d56bf53a40586057d973affc63f7b661c1
KaleemAhmed123 <shaamidreez@gmail.com>
2023-04-10 20:00:35 +0530

    Rank and Links added.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -13,11 +13,11 @@ const CryptoDetails = () => {
   let { getCoinData, coinData: data, currency } = useContext(CryptoContext);
 
   useLayoutEffect(() => {
-    getCoinData(coinId); // calling api from here
+    getCoinData(coinId); // calling api from here on change of coinID
   }, [coinId]);
 
   const close = () => {
-    navigate(".."); // last page
+    navigate("..");
   };
 
   return ReactDOM.createPortal(
@@ -274,26 +274,139 @@ const CryptoDetails = () => {
             </div>
             {/* Wrpapper Left End */}
 
-            {/* Right Start */}
-
+            {/* Right Start- Chart and Links */}
             <div className="flex flex-col w-[55%] h-full pl-3 ">
               <Chart id={data.id} />
+
+              {/* Rank Part */}
+              <div className="flex flex-col mt-4">
+                <h3 className="text-white py-1">
+                  <span className="text-gray-100 capitalize mr-1">
+                    market cap rank:{" "}
+                  </span>{" "}
+                  {data.market_cap_rank}{" "}
+                </h3>
+
+                <h3 className="text-white py-1">
+                  <span className="text-gray-100 capitalize mr-1">
+                    coinGecko rank:{" "}
+                  </span>{" "}
+                  {data.coingecko_rank}{" "}
+                </h3>
+                <h3 className="text-white py-1">
+                  <span className="text-gray-100 capitalize mr-1">
+                    coinGecko score:{" "}
+                  </span>{" "}
+                  {data.coingecko_score}{" "}
+                </h3>
+              </div>
+              {/* end */}
             </div>
 
-            {/* Links ... */}
+            {/* All Links at bottomRight of Right Part of popUp noFlex */}
+            <div className="absolute bottom-8 right-8 flex items-center">
+              {/* Github */}
+              {data.links.repos_url.github[0] && (
+                <a
+                  className="text-lg px-1"
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href={data.links.repos_url.github[0]}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                    style={{
+                      msTransform: "rotate(360deg)",
+                      WebkitTransform: "rotate(360deg)",
+                      transform: "rotate(360deg)",
+                    }}
+                  >
+                    <path
+                      fill="currentColor"
+                      className="fill-cyan"
+                      fillRule="evenodd"
+                      d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z"
+                      clipRule="evenodd"
+                    />
+                    <path fill="rgba(0, 0, 0, 0)" d="M0 0h24v24H0z" />
+                  </svg>
+                </a>
+              )}
+              {/* TwitterSpace */}
+              {data.links.twitter_screen_name && (
+                <a
+                  className="text-lg px-1"
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href={`https://twitter.com/${data.links.twitter_screen_name}`}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 1024 1024"
+                    style={{
+                      msTransform: "rotate(360deg)",
+                      WebkitTransform: "rotate(360deg)",
+                      transform: "rotate(360deg)",
+                    }}
+                  >
+                    <path
+                      fill="currentColor"
+                      className="fill-cyan"
+                      d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm215.3 337.7c.3 4.7.3 9.6.3 14.4 0 146.8-111.8 315.9-316.1 315.9-63 0-121.4-18.3-170.6-49.8 9 1 17.6 1.4 26.8 1.4 52 0 99.8-17.6 137.9-47.4-48.8-1-89.8-33-103.8-77 17.1 2.5 32.5 2.5 50.1-2a111 111 0 0 1-88.9-109v-1.4c14.7 8.3 32 13.4 50.1 14.1a111.13 111.13 0 0 1-49.5-92.4c0-20.7 5.4-39.6 15.1-56a315.28 315.28 0 0 0 229 116.1C492 353.1 548.4 292 616.2 292c32 0 60.8 13.4 81.1 35 25.1-4.7 49.1-14.1 70.5-26.7-8.3 25.7-25.7 47.4-48.8 61.1 22.4-2.4 44-8.6 64-17.3-15.1 22.2-34 41.9-55.7 57.6z"
+                    />
+                    <path fill="rgba(0, 0, 0, 0)" d="M0 0h1024v1024H0z" />
+                  </svg>
+                </a>
+              )}
+              {data.links.facebook_username && (
+                <a
+                  className="text-lg px-1"
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href={`https://facebook.com/${data.links.facebook_username}`}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                    style={{
+                      msTransform: "rotate(360deg)",
+                      WebkitTransform: "rotate(360deg)",
+                      transform: "rotate(360deg)",
+                    }}
+                  >
+                    <path
+                      fill="currentColor"
+                      className="fill-cyan"
+                      fillRule="evenodd"
+                      d="M0 12.067C0 18.033 4.333 22.994 10 24v-8.667H7V12h3V9.333c0-3 1.933-4.666 4.667-4.666.866 0 1.8.133 2.666.266V8H15.8c-1.467 0-1.8.733-1.8 1.667V12h3.2l-.533 3.333H14V24c5.667-1.006 10-5.966 10-11.933C24 5.43 18.6 0 12 0S0 5.43 0 12.067Z"
+                      clipRule="evenodd"
+                    />
+                    <path fill="rgba(0, 0, 0, 0)" d="M0 0h24v24H0z" />
+                  </svg>
+                </a>
+              )}
+            </div>
           </div>
         ) : (
-          // loading indicator
+          // Loader
           <div className="w-full min-h-[60vh] h-full flex justify-center items-center">
             <div
               className="w-8 h-8 border-4 border-cyan rounded-full
-             border-b-gray-200 animate-spin "
+             border-b-gray-200 animate-spin"
               role="status"
             />
             <span className="ml-2">please wait...</span>
           </div>
         )}
       </div>
+      {/* popUp End */}
     </div>,
     document.getElementById("model")
     // secondParam is Where we wanna render (freecodecamp blog refer)
