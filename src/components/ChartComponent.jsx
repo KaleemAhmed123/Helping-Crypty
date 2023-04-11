b952a9639a2980b55184825db24cd4ee9078911c
KaleemAhmed123 <shaamidreez@gmail.com>
2023-04-11 23:03:10 +0530

    ChartUI library setup done (cpyd).

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const ChartComponent = () => {
-  return <div>ChartComponent</div>;
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const ChartComponent = ({ data, currency, type }) => {
+  return (
+    <ResponsiveContainer height={"90%"}>
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#14ffec"
+          strokeWidth={"1px"}
+        />
+        <CartesianGrid stroke="#323232" />
+        <XAxis dataKey="date" hide />
+        <YAxis dataKey={type} hide domain={["auto", "auto"]} />
+        <Tooltip
+          //   content={<CustomTooltip />}
+          currency={currency}
+          cursor={false}
+          wrapperStyle={{ outline: "none" }}
+        />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default ChartComponent;
